927c6e125fea9a61f70e760477f253719f405a84
Omri Ben-Ary <omri@cs.technion.ac.il>
2024-10-02 16:54:56 +0300

    start VAD

---
+++ .ipynb_checkpoints/VAD_Trainer-checkpoint.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def reconstruction_loss(x, x_rec):
+    """
+    :param x: the original images
+    :param x_rec: the reconstructed images
+    :return: the reconstruction loss
+    """
+    return torch.norm(x - x_rec) / torch.prod(torch.tensor(x.shape))
+
+def elbo_loss(x, x_rec, mu, log_var):
+    """
+    :param x: the original images
+    :param x_rec: the reconstructed images
+    :return: the elbo loss
+    """
+    kl_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+    return reconstruction_loss(x, x_rec) + kl_loss
+    
+
+class VAD_Trainer:
+    def __init__(self, var_decoder, dataloader, latent_dim=256, device='cpu', lr=1e-3):
+        """
+        Initialize the Trainer class.
+        
+        :param var_decoder: The decoder model (PyTorch nn.Module)
+        :param latent_dim: The size of the latent space
+        :param dataloader: The DataLoader for Fashion MNIST dataset
+        :param device: 'cpu' or 'cuda' (for GPU support)
+        :param lr: Learning rate for the optimizer
+        """
+        self.var_decoder = var_decoder.to(device)
+        self.latent_dim = latent_dim
+        self.dataloader = dataloader
+        self.device = device
+        self.latents = torch.nn.Parameter(torch.randn(len(self.dataloader.dataset), self.latent_dim).to(self.device))
+        
+        # Optimizer
+        self.optimizer = optim.Adam(list(self.var_decoder.parameters()) + [self.latents], lr=lr)
+        
+        # Loss function
+        self.loss = elbo_loss
+        
+    def train_epoch(self):
+        self.var_decoder.train()
+        running_loss = 0.0
+        
+        for batch_idx, (_, x) in enumerate(self.dataloader):
+            images = x.to(self.device)
+            batch_size = images.size(0)
+            z = self.latents[batch_idx * batch_size : (batch_idx + 1) * batch_size, :]
+            
+            reconstructed_images, mu, log_var = self.var_decoder(z)
+            
+            loss = self.loss(images, reconstructed_images, mu, log_var)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            running_loss += loss.item()
+        
+        # Average loss over the epoch
+        epoch_loss = running_loss / len(self.dataloader)
+        return epoch_loss
+    
+    def train(self, num_epochs=1000, early_stopping=None):
+        """
+        Train the AutoDecoder for multiple epochs.
+        
+        :param num_epochs: Number of epochs to train the model
+        """
+        losses=list()
+        best_loss = None
+        for epoch in range(num_epochs):
+            epoch_loss = self.train_epoch()
+            losses.append(epoch_loss)
+            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}")
+            
+            if best_loss is None or epoch_loss < best_loss:
+                no_improvement = 0
+                best_loss = epoch_loss
+            else:
+                no_improvement += 1
+                if early_stopping is not None and no_improvement >= early_stopping:
+                    break
+
+        return losses
--- VariationalAutoDecoder.py
+++ VariationalAutoDecoder.py
@@ -2,4 +2,57 @@ import torch
 import torch.nn as nn
 
 class VariationalAutoDecoder(nn.Module):
-    pass
+    def __init__(self, mu_layers=None, var_layers=None):
+        super().__init__()
+
+        if mu_layers is None:
+            self.mu_net = nn.Sequential(
+                nn.Linear(256, 512),
+                nn.ReLU(),
+                nn.Linear(512, 256),
+            )
+        else:
+             self.mu_net = mu_layers
+
+        if var_layers is None:
+            self.log_var_net = nn.Sequential(
+                nn.Linear(256, 512),
+                nn.ReLU(),
+                nn.Linear(512, 256),
+            )
+        else:
+            self.log_var_net = var_layers
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(1024, 2048),
+            nn.BatchNorm1d(2048),
+            nn.ReLU(),
+            nn.Linear(2048, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 784)
+        )
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std  # Reparameterization trick
+    
+    def forward(self, latent_vec):
+        mu = self.mu_net(latent_vec)
+        log_var = self.log_var_net(latent_vec) 
+        z = self.reparameterize(mu, log_var)
+        
+        rec_image = self.decoder(z)
+        rec_image = rec_image.view(-1, 28, 28)
+        return rec_image, mu, log_var
+
+
+
